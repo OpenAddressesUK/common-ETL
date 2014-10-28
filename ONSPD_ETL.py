38dbc4f14a102d45b332c136eaea0f7cb60052fc
John Murray <john@johnmurray.info>
2014-10-28 14:20:56 +0000

    Update ONSPD_ETL.py
    Changes to reflect database restructure and cache retired postcodes in memory to build changes table.

--- ONSPD_ETL.py
+++ ONSPD_ETL.py
@@ -30,9 +30,20 @@ cur = dbConn.cursor()
 
 query = "TRUNCATE TABLE  `ONSPD`;"
 cur.execute(query)
-fields = ["pcds", "usertype", "EA", "NO", "osgrdind", "ctry", "current"]
 
-bi = BulkInsert(cur,"ONSPD",fields)
+ONSPD_fields = ["pcds", "usertype", "EA", "NO", "osgrdind", "ctry", "current"]
+
+ONSPD_bi = BulkInsert(cur,"ONSPD",ONSPD_fields)
+
+query = "TRUNCATE TABLE  `ONSPD_Changes`;"
+cur.execute(query)
+
+change_fields = ["curr_pcds", "term_pcds"]
+
+change_bi = BulkInsert(cur,"ONSPD_Changes",change_fields)
+
+ret_pcds = []
+cur_pcds = []
 
 for file in glob.glob("ONSPD*.csv"):
     print file
@@ -42,19 +53,40 @@ for file in glob.glob("ONSPD*.csv"):
     reader.fieldnames = [field.strip() for field in reader.fieldnames]
     for row in reader:
         nrecs += 1
-        # print row
         if (nrecs % 10000) == 0:
             print "Records read: " + str(nrecs)
         if 'oseast1m' in row:
+            if row['doterm'] > "":
+                current = '0'
+                if row['oseast1m'] > '':
+                    ret_pcds.append((row['pcds'], row['oseast1m'], row['osnrth1m']))
+            else:
+                current = '1'
+                if row['oseast1m'] > '':
+                    cur_pcds.append((row['pcds'], row['oseast1m'], row['osnrth1m']))
             if row['oseast1m'] > '':
-                if row['doterm'] > "":
-                    current = '0'
-                else:
-                    current = '1'
-                lines = [row['pcds'],row['usertype'], row['oseast1m'], row['osnrth1m'], row['osgrdind'], row['ctry'][0], current]
-                bi.addRow(lines)
+                lines = [row['pcds'], row['usertype'], row['oseast1m'], row['osnrth1m'], row['osgrdind'], row['ctry'][0], current]
+            else:
+                lines = [row['pcds'], row['usertype'], "NULL", "NULL", row['osgrdind'], row['ctry'][0], current]
+            ONSPD_bi.addRow(lines)
     print "Records read: " + str(nrecs)
     csvfile.close()
-bi.close() 
+ONSPD_bi.close() 
+dbConn.commit()
+
+print "Writing changes to database"
+nrecs = 0
+nwrit = 0
+for term_pc in ret_pcds:
+    nrecs += 1
+    query = "SELECT `pcds` FROM `ONSPD` WHERE `current` = '1' AND `EA` = '" + term_pc[1] + "' AND `NO` = '" + term_pc[2] + "';"
+    cur.execute(query)
+    if (nrecs % 10000) == 0:
+        print "Changes read: " + str(nrecs) + " written: " + str(nwrit)
+    if cur.rowcount == 1:
+        nwrit += 1
+        for row in cur.fetchall():
+           change_bi.addRow([row[0],term_pc[0]])
+change_bi.close() 
 dbConn.commit()
 dbConn.close()
