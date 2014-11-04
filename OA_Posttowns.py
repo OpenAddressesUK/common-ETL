d5f3751d5583802f196254567ce48bcc29750a21
John Murray <john@johnmurray.info>
2014-11-04 12:07:33 +0000

    Changed to use ETL database
    Changed to use the ETL database rather than csv file.

--- OA_Posttowns.py
+++ OA_Posttowns.py
@@ -10,10 +10,30 @@
 # Purpose       Script to Extract posttowns from Wikipedia
 #
 
+import ConfigParser
 from HTMLParser import HTMLParser
 import urllib
 import urllib2
 import csv
+from bulkinsert import *
+
+# Read database configuration from config file
+config = ConfigParser.ConfigParser()
+config.read("oa_alpha_etl.cnf")
+username = config.get('database', 'username')
+password = config.get('database', 'password')
+hostname = config.get('database', 'hostname')
+database = config.get('database', 'database')
+
+dbConn = MySQLdb.connect(host=hostname,user=username,passwd=password,db=database)
+cur = dbConn.cursor() 
+
+query = "TRUNCATE TABLE  `Posttowns`;"
+cur.execute(query)
+
+fields = ["pcarea", "town"]
+
+bi = BulkInsert(cur,"Posttowns",fields)
 
 url = "http://en.wikipedia.org/wiki/List_of_post_towns_in_the_United_Kingdom"
 
@@ -57,10 +77,6 @@ class TownExtractor(HTMLParser):
             self.towns.append([self.pcarea, data.decode('ISO-8859-1').encode('ascii','ignore').upper()])
             self.towntag = False
             
-csvout = open('OA_Posttowns.csv', 'w') 
-writer = csv.writer(csvout, delimiter=',',quotechar='"', quoting=csv.QUOTE_ALL)
-writer.writerow(['Area', 'Posttown'])
-
 response = urllib2.urlopen(url)
 html = response.read()
 
@@ -69,8 +85,12 @@ print "Parsing Wikipedia Page " + url
 parser = TownExtractor()
 parser.feed(html)
 
-writer.writerows(parser.towns)
+for t in parser.towns:
+    print t
+    bi.addRow(t)
 
-csvout.close()
+bi.close()
+dbConn.commit()
+dbConn.close()
 
 print str(len(parser.towns)) + " posttowns written to database."
