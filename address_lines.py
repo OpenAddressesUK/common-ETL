6b923d43ad34b22bc010bc66eb08bf9db84c2c0a
John Murray <john@johnmurray.info>
2014-11-05 16:37:05 +0000

    Changes to parsing to use MBR buffer
    Changes to parsing rules to use MBR on street validation.

--- address_lines.py
+++ address_lines.py
@@ -24,13 +24,15 @@ class AddressLines:
         self.lines = 0                  # Initialise number of elements to 0
         self.towns = {}                 # Initialise towns object to empty
         self.streets = {}               # Initialise street types
-        self.townpos = [-1, -1, -1]     # Position of town in address [line, subline, word]
+        self.townpos = [-1, -1]         # Position of town in address [line, subline, word]
         self.aons = []                  # Position of addressable objects within the address
         self.elements = collections.OrderedDict()   # Formatted address elements
+        self.pcpoint = (0,0)            # Postcode coordinates
+        self.cur = cur
         query = "SELECT `pcarea`, `town` FROM `Posttowns`;"
-        cur.execute(query)
+        self.cur.execute(query)
         nrecs = 0
-        for row in cur.fetchall() :
+        for row in self.cur.fetchall() :
             words = [w.translate(None,string.punctuation) for w in re.split(' |-',row[1])]
             key = words[0][:4]
             if row[0] not in self.towns:
@@ -39,15 +41,17 @@ class AddressLines:
                 self.towns[row[0]][key] = []
             self.towns[row[0]][key].append([row[1],words])
             nrecs += 1
-        print "Records read: " + str(nrecs)
-        pprint(self.towns)
+        # print "Records read: " + str(nrecs)
+        # pprint(self.towns)
         
-    def setAddress(self, address, pcarea):
+    def setAddress(self, address, pc):
         # self.address = address
         self.address = []
         for line in address:
             self.address.extend(line.split(","))
-        self.pcarea = pcarea
+        self.pcarea = pc.getArea("S")
+        self.pcpoint = pc.centroid
+        self.postcode = pc.getPostcode("S")
         self.townpos = [-1, -1, -1]
         self.elements = {}
         
@@ -57,63 +61,111 @@ class AddressLines:
         
     def getTown(self):
         lineno = 0
-        for line in reversed(self.address):
-            lineno += 1
-            sublines = line.split(",")
-            sublineno = 0
-            for subline in sublines:
-                sublineno += 1
-                words = re.findall(r"[\w']+",subline)
-                # print "Words"
-                # print words
-                for i in range(0,len(words)):
-                    keys = self.getTownKey(words[i])
-                    # print "Keys"
-                    # print keys
-                    if self.pcarea in self.towns:
-                        if keys[0] in self.towns[self.pcarea]:
-                            matchwords = 0
-                            town = -1
-                            # print self.towns[self.pcarea][keys[0]]
-                            for j in range(0,len(self.towns[self.pcarea][keys[0]])):
-                                # print "loop j="+str(j)
-                                # print self.towns[self.pcarea][keys[0]][j][0]
-                                # print self.towns[self.pcarea][keys[0]][j][1]
-                                if self.towns[self.pcarea][keys[0]][j][1][0] == keys[1][0]:
-                                    ntwords = len(self.towns[self.pcarea][keys[0]][j][1])
-                                    # print "ntwords ="+str(ntwords)
-                                    if ntwords > matchwords:
-                                        m = 1
-                                        for k in range(1,ntwords):
-                                            # print "loop k="+str(k)
-                                            # print self.towns[self.pcarea][keys[0]][j][1][k]
-                                            if (i+k) < len(words) and words[i+k][:2].upper() == self.towns[self.pcarea][keys[0]][j][1][k][:2]:
-                                                m += 1
-                                        if m > matchwords:
-                                            matchwords = m
-                                            town = j
-                            if town > -1:
-                                self.townpos = [len(self.address)-lineno, sublineno, i]
-                                self.elements['town'] = self.towns[self.pcarea][keys[0]][town][0]
-                                return self.towns[self.pcarea][keys[0]][town][0]
+        # Reverse search for town
+        for i in range(len(self.address),0,-1):
+            # print i
+            words = re.findall(r"[\w']+",self.address[i-1])
+            # print "Words"
+            # print words
+            for j in range(0,len(words)):
+                keys = self.getTownKey(words[j])
+                # print "Keys"
+                # print keys
+                if self.pcarea in self.towns:
+                    if keys[0] in self.towns[self.pcarea]:
+                        matchwords = 0
+                        town = -1
+                        # print self.towns[self.pcarea][keys[0]]
+                        for k in range(0,len(self.towns[self.pcarea][keys[0]])):
+                            if self.towns[self.pcarea][keys[0]][k][1][0] == keys[1][0]:
+                                ntwords = len(self.towns[self.pcarea][keys[0]][k][1])
+                                if ntwords > matchwords:
+                                    m = 1
+                                    for l in range(1,ntwords):
+                                        if (j+l) < len(words) and words[j+l][:2].upper() == self.towns[self.pcarea][keys[0]][k][1][l][:2]:
+                                            m += 1
+                                    if m > matchwords:
+                                        matchwords = m
+                                        town = k
+                                        self.townpos = [i-1, j]
+                        if town > -1:                            
+                            self.elements['town'] = self.towns[self.pcarea][keys[0]][town][0]
+                            if j == 0:
+                                self.address[i-1] = ''
+                            else:
+                                self.address[i-1] = string.join(words[0:j], " ")
+                            for k in range (i,len(self.address)):
+                                self.address[k] = ''
+                            return self.towns[self.pcarea][keys[0]][town][0]
         
         return ''
+        
+    def getStreet(self):
+        if self.pcpoint[0] > 0:
+            # minx = str(self.pcpoint[0]-1000)
+            # maxx = str(self.pcpoint[0]+1000)
+            # miny = str(self.pcpoint[1]-1000)
+            # maxy = str(self.pcpoint[1]+1000)
+            # query = "SELECT `Name` FROM `OS_Locator` WHERE `Name` > '' AND `Centx` BETWEEN "+minx+" AND "+maxx+" AND `Centy` BETWEEN "+miny+" AND "+maxy+";"
+            query = "SELECT `Name` FROM `OS_Locator` WHERE `Name` > '' AND CONTAINS(`MBR25` ,POINT( "+str(self.pcpoint[0])+", "+str(self.pcpoint[1])+"));"
+            self.cur.execute(query)
+            max = len(self.address)
+            streets = []
+            streetlines = []
+            for street in self.cur.fetchall():
+                for i in range(0,max):  # Look for number at start of word
+                    if street[0] in self.address[i]:
+                        if street[0] not in streets:
+                            streets.append(street[0])
+                            streetlines.append(i)
+            # print self.address   
+            # print streets
+            if len(streets) == 1:
+                self.address[streetlines[0]] = self.address[streetlines[0]].replace(streets[0],"").strip()
+                self.elements['street'] = streets[0]
+                if streetlines[0] < (len(self.address) - 1):
+                    if self.address[streetlines[0]+1] != '' and not any(char.isdigit() for char in self.address[streetlines[0]+1]):
+                        self.elements['locality'] = self.address[streetlines[0]+1]
+                        self.address[streetlines[0]+1] = ''
+            elif len(streets) > 1:
+                max = len(streets)
+                if streets[max-1] in streets[max-2] and streetlines[max-1] == streetlines[max-2]:
+                    self.elements['street'] = streets[max-2]
+                    self.address[streetlines[max-2]] = self.address[streetlines[max-2]].replace(streets[max-2],"").strip()
+                    if streetlines[max-2] < (len(self.address) - 1):
+                        if self.address[streetlines[max-2]+1] != '' and not any(char.isdigit() for char in self.address[streetlines[max-2]+1]):
+                            self.elements['locality'] = self.address[streetlines[max-2]+1]
+                            self.address[streetlines[max-2]+1] = ''
+                elif streets[max-2] in streets[max-1] and streetlines[max-2] == streetlines[max-1]:                    
+                    self.elements['street'] = streets[max-1]
+                    self.address[streetlines[max-1]] = self.address[streetlines[max-1]].replace(streets[max-1],"").strip()
+                    if streetlines[max-1] < (len(self.address) - 1):
+                        if self.address[streetlines[max-1]+1] != '' and not any(char.isdigit() for char in self.address[streetlines[max-1]+1]):
+                            self.elements['locality'] = self.address[streetlines[max-1]+1]
+                            self.address[streetlines[max-1]+1] = ''
+                else:
+                    self.elements['street'] = streets[max-1] + ", " + streets[max-2]
+                    self.address[streetlines[max-1]] = self.address[streetlines[max-1]].replace(streets[max-1],"").strip()                    
+                    self.address[streetlines[max-2]] = self.address[streetlines[max-2]].replace(streets[max-2],"").strip()
+                    if streetlines[max-1] < (len(self.address) - 1):
+                        if self.address[streetlines[max-1]+1] != '' and not any(char.isdigit() for char in self.address[streetlines[max-1]+1]):
+                            self.elements['locality'] = self.address[streetlines[max-1]+1]
+                            self.address[streetlines[max-1]+1] = ''
+            # print self.address
+            # print self.elements
+                
         
     def getAons(self):
         self.aons = []
         if self.townpos[0] == -1:
             self.getTown()
-        if self.townpos[0] == -1:
-            max = len(self.address)
-        else:
-            max = self.townpos[0]
+        max = len(self.address)
         for i in range(0,max):      # Look for number at start of word
-            sublines = self.address[i].split(",")
-            for j in range(0,len(sublines)):
-                words = sublines[j].split()
-                for k in range(0,len(words)):
-                    if words[k][0].isdigit():
-                        self.aons.append([i,j,k,string.join(words[0:k]," "),words[k],string.join(words[k+1:]," ")])
+            if self.address[i] != '':
+                words = self.address[i].split()
+                for j in range(0,len(words)):
+                    if words[j][0].isdigit():
+                        self.aons.append([i,j,string.join(words[0:j]," "),words[j],string.join(words[j+1:]," ")])
         pos = 1
         if self.aons == []:
             self.aons.append([0,0,0,"",self.address[0],""])
@@ -122,22 +174,8 @@ class AddressLines:
             pos = self.aons[0][0] + 1
             if self.aons[0][0] > 0:
                 self.elements['saon'] = self.address[0]
-            if self.aons[0][5] != "":
-                self.elements['street'] = self.aons[0][5]
-            else:
-                if (self.aons[0][0]+1) < max:
-                    self.elements['street'] = self.address[self.aons[0][0]+1]
-                pos = self.aons[0][0] + 2
         elif len(self.aons) >= 2:
             self.elements['paon'] = (self.aons[1][3]+" "+self.aons[1][4]).strip()
             pos = self.aons[0][0] + 1
             self.elements['saon'] = self.address[0].strip()
-            if self.aons[1][5] != "":
-                self.elements['street'] = self.aons[1][5].strip()
-            else:
-                self.elements['street'] = self.address[self.aons[1][0]+1].strip()
-                pos = self.aons[0][0] + 2        
-        if pos < max:
-            if self.address[pos] > "":
-                self.elements['locality'] = self.address[pos]
         return self.aons
