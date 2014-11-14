ddca2636889a18baa5eb2a070689ad5d306f48e2
James Smith <james@floppy.org.uk>
2014-11-14 12:14:13 +0000

    Read CSV as a stream, not all at once

--- CH_Bulk_Extractor.py
+++ CH_Bulk_Extractor.py
@@ -28,7 +28,6 @@ import collections
 def storeAddresses(out):
     if len(out) > 0:                # Check there is data to write
         data = json.dumps(out, indent=1)
-        print data
         headers = { 'ACCESS_TOKEN' : apitoken }
         url = apiurl
         req = urllib2.Request(url, data, headers)
@@ -45,7 +44,7 @@ def storeAddresses(out):
             except URLError as e:
                sys.exit("Fatal error - Ingester API URL Error: " + str(e.code) + " - " + e.reason)
         if ntries >= max_tries:
-            sys.exit("Fatal error - Ingester API HTTP Error max tries reached ("+str(ntries)+"): " + str(err.code) + " - " + err.reason)
+            sys.exit ("Fatal error - Ingester API HTTP Error max tries reached ("+str(ntries)+")")
 
 
 # Process a single file
@@ -58,54 +57,59 @@ def process_file(file):
     csvfile = open(file, 'rb')
     companyreader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
     companyreader.fieldnames = [field.strip() for field in companyreader.fieldnames]
-    rows = list(companyreader)
 
-    for i in xrange(0, len(rows), 100):
+    run = True
+    while run: # Loop until we run out of data
         out = {}
         out['addresses'] = []
 
-        for row in rows[i:i+100]:
-            nrecs += 1
-            if (nrecs % 50) == 0:
-                print "Records read: " + str(nrecs)
-                elapsed = time.time() - start_time
-                print str(elapsed) + " secs elapsed"
-                print str((60 * nrecs) / elapsed) + " recs/min"
+        try:
+            for rowcount in range(0,100):
+                row = companyreader.next()
+                nrecs += 1
+                if (nrecs % 50) == 0:
+                    print "Records read: " + str(nrecs)
+                    elapsed = time.time() - start_time
+                    print str(elapsed) + " secs elapsed"
+                    print str((60 * nrecs) / elapsed) + " recs/min"
 
-            if 'RegAddress.PostCode' in row:
-                if row['RegAddress.PostCode'] > '':
-                    pc = Postcode(row['RegAddress.PostCode'],cur)
-                    if pc.current != -1:
-                        lines = [row['RegAddress.AddressLine1'], row['RegAddress.AddressLine2'], row['RegAddress.PostTown'], row['RegAddress.County']]
-                        a.setAddress(lines,pc)
+                if 'RegAddress.PostCode' in row:
+                    if row['RegAddress.PostCode'] > '':
+                        pc = Postcode(row['RegAddress.PostCode'],cur)
+                        if pc.current != -1:
+                            lines = [row['RegAddress.AddressLine1'], row['RegAddress.AddressLine2'], row['RegAddress.PostTown'], row['RegAddress.County']]
+                            a.setAddress(lines,pc)
 
-                        if a.getTown() != '':
-                            # Future code for inference - not active in alpha
-                            # try:
-                            #    companywriter.writerow([pc.getPostcode("S"), a.getTown(), pc.getSector("S")])
-                            # except:
-                            #     print row
-                            #    sys.exit("Sector failure")
-                            a.getStreet()
-                            a.getAons()
-                            address = collections.OrderedDict()
-                            address['address'] = a.elements
-                            address['address']['postcode'] = collections.OrderedDict()
-                            address['address']['postcode']['name'] = pc.getPostcode("S")
-                            address['address']['postcode']['geometry'] = collections.OrderedDict()
-                            address['address']['postcode']['geometry']['type'] = 'Point'
-                            address['address']['postcode']['geometry']['coordinates'] = [pc.centroid[1], pc.centroid[0]]
-                            # Next line for future use for inference
-                            # out['address']['sector'] = pc.getSector("S")
-                            address['provenance'] = {}
-                            address['provenance']['executed_at'] = datetime.datetime.today().strftime("%Y-%m-%dT%H:%M:%SZ")
-                            address['provenance']['url'] = "http://download.companieshouse.gov.uk/en_output.html"
-                            address['provenance']['filename'] = file
-                            address['provenance']['record_no'] = str(nrecs)
-                            # print out
-                            # print json.dumps(out, indent=1)
-                            # print lines
-                            out['addresses'].append(address)
+                            if a.getTown() != '':
+                                # Future code for inference - not active in alpha
+                                # try:
+                                #    companywriter.writerow([pc.getPostcode("S"), a.getTown(), pc.getSector("S")])
+                                # except:
+                                #     print row
+                                #    sys.exit("Sector failure")
+                                a.getStreet()
+                                a.getAons()
+                                address = collections.OrderedDict()
+                                address['address'] = a.elements
+                                address['address']['postcode'] = collections.OrderedDict()
+                                address['address']['postcode']['name'] = pc.getPostcode("S")
+                                address['address']['postcode']['geometry'] = collections.OrderedDict()
+                                address['address']['postcode']['geometry']['type'] = 'Point'
+                                address['address']['postcode']['geometry']['coordinates'] = [pc.centroid[1], pc.centroid[0]]
+                                # Next line for future use for inference
+                                # out['address']['sector'] = pc.getSector("S")
+                                address['provenance'] = {}
+                                address['provenance']['executed_at'] = datetime.datetime.today().strftime("%Y-%m-%dT%H:%M:%SZ")
+                                address['provenance']['url'] = "http://download.companieshouse.gov.uk/en_output.html"
+                                address['provenance']['filename'] = file
+                                address['provenance']['record_no'] = str(nrecs)
+                                # print out
+                                # print json.dumps(out, indent=1)
+                                # print lines
+                                out['addresses'].append(address)
+        except StopIteration as e:
+            run = False
+            pass
         storeAddresses(out)
 
     print "Records read: " + str(nrecs)
